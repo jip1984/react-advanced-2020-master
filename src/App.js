5407ac7daac028036c6e9bf72c2654a02c697705
jon <jonprosser84@gmail.com>
2021-07-20 20:23:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import UseStateObject from './tutorial/1-useState/setup/4-useState-object';
+
+function App() {
+  return (
+    <div className='container'>
+      <UseStateObject />
+    </div>
+  );
+}
+
+export default App
---
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+const ErrorExample = () => {
+
+
+  let title = 'random title';
+  const handleClick = () => {
+    console.log(title);
+  };
+
+  return (
+    <>
+      <h2>{title}</h2>;
+      <button type="button" className="btn" onClick={handleClick}>Change Title</button>
+    </>
+  );
+};
+
+export default ErrorExample;
